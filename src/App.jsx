0b73c24c65c7a8535a42533b2651525fb424352c
Athrysd <221656567+Athrysd@users.noreply.github.com>
2025-12-11 13:16:30 +0700

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -89,10 +89,10 @@ const App = () => {
   return (
     <div
       id="home"
-      className="bg-primary pt-16 md:pt-29 min-h-screen flex sm:flex-row justify-between items-center gap-0 sm:gap-10 flex-col-reverse px-5 sm:px-10"
+      className="bg-primary overflow-hidden pt-16 md:pt-29 min-h-screen flex sm:flex-row justify-between items-center gap-0 sm:gap-10 flex-col-reverse px-5 sm:px-10"
     >
+      <div className="hiasan absolute hidden sm:block -top-80 left-100 rotate-30 z-20 rounded-2xl bg-accent w-3xl h-[500px]"></div>
       {/* Kiri - Text */}
-
       <div className="kiri sm:w-1/2 p-5 sm:p-10 flex flex-col justify-center gap-10 items-start animate-fade-slide-up">
         <h1
           className="text-2xl text-center font-bold text-putih leading-snug md:text-3xl sm:text-2xl sm:text-start"
@@ -122,7 +122,7 @@ const App = () => {
       </div>
 
       {/* Kanan - Slider */}
-      <div className="kanan w-full sm:w-1/2 max-w-md relative mt-5 px-4" data-aos="fade-left">
+      <div className="kanan w-full z-50 sm:w-1/2 max-w-md relative mt-5 px-4" data-aos="fade-left">
         <div className="w-full">
           <Slider {...settings}>
             {listGambar.map((item, idx) => (
